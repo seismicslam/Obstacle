99289d89cded2f790a55415ed264e8624339e0d4
unknown <RHS@RHS-M2A-32.rusd.local>
2022-10-11 15:44:40 -0700

    Day 3

---
+++ My project/Assets/PlayerMover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    float xValue = 0.01f;
+    float yValue = 0.0f;
+    float zValue = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
