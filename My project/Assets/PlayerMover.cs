1bc5d14d0de22eb8ef8027b7125894063df7dfd5
unknown <RHS@RHS-M2A-32.rusd.local>
2022-10-25 15:39:59 -0700

    Camera Addition

--- My project/Assets/PlayerMover.cs
+++ My project/Assets/PlayerMover.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
 
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        float moveSpeed = 11f;
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
     }
+
 }
